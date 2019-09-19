fe79f33842f5cf2a19bdfe0c478ef899ec9930a5
Martins-O-U <martinsonyedikachi@gmail.com>
2019-09-19 14:44:39 +0100

    create reat App as well as its Dependencies created.

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './App.css';
+import Form from './Form';
+
+import uuid from 'uuid';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
